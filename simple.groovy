61110267e2dd0d29f452611b22c2b5c183a99dbe
Viktar Chemko <viktar_chemko@epam.com>
2020-08-09 15:48:36 +0300

    first commit

---
+++ simple.groovy
@@ -0,0 +1,25 @@
+#!groovy
+// Check ub1 properties
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    agent { 
+        label 'master'
+        }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+                sh 'ssh test@ecsb003009ed.epam.com \'hostname\''
+            }
+        }
+        stage("Second step") {
+            steps {
+                sh 'ssh test@ecsb003009ed.epam.com hostname \'uptime\''
+            }
+        }
+    }
+}
